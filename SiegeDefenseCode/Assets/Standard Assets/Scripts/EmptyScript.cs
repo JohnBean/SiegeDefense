6c94bba09f53d8c983ecefbe8263e8826d5831b5
Bean <shadowfox683@gmail.com>
2013-03-25 19:12:30 -0400

    Cannonball added, basic blocks added
    The cannonball has been created. It can have its angle and shot power
    adjusted to shoot where we want it. I've also made some base blocks. We
    may want to add a 1x1 block and a T block as well.

--- SiegeDefenseCode/Assets/Standard Assets/Scripts/EmptyScript.cs
+++ SiegeDefenseCode/Assets/Standard Assets/Scripts/EmptyScript.cs
@@ -1,11 +1,11 @@
 using UnityEngine;
 using System.Collections;
 
-public class EmptyScript : MonoBehaviour {
-
+public class GameEngine : MonoBehaviour {
+	int state=0;//states    0-Opening splash screen.    1-Build Mode    2-Attack mode    3-Win state    4-Lose state
 	// Use this for initialization
 	void Start () {
-	
+		
 	}
 	
 	// Update is called once per frame
